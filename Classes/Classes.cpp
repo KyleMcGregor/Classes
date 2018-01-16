568e42393c6da4dfd7b6442054d50eb9f2bbbdad
Kyle McGregor <kyle.mcgregor@hotmail.com>
2018-01-15 23:09:03 -0500

    Just getting started, read the first ferw pages of chapter 3 and made some notes here.

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -9,3 +9,35 @@ int main()
     return 0;
 }
 
+ 
+class complex
+{
+	double re, im; //representation, two doubles
+public:
+	complex(double i, double i) :re{ r }, im{ i } {} //construct complex from two scalars
+	complex(double r) : re{ r }, im{ 0 } {}
+	complex() : re{ 0 }, im{ 0 } {}
+
+	double real() const { return re; }
+	void real(double d) { re = d; }
+	double imag() const { return im; }
+	void imag(double d) { im = d; }
+
+	complex& operator+=(complex z) { re += z.re, im += z.im; return *this; }
+
+	complex& operator-=(complex z) { re -= z.re, im -= z.im;return *this }
+
+	~complex();
+
+private:
+
+};
+
+complex::complex()
+{
+}
+
+complex::~complex()
+{
+}
+
